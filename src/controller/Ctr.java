65fc3f0b226d00d01a2a430c2e0c49babc3cb80a
joselu4112 <jcaragu537@g.educaand.es>
2024-11-22 16:00:04 +0100

    controller

--- src/controller/Ctr.java
+++ src/controller/Ctr.java
@@ -1,5 +1,19 @@
 package controller;
 
+import model.Cuenta;
+
 public class Ctr {
 
+	
+	private Lista lista;
+	
+	public Ctr() {
+		lista=new Lista();
+		
+	}
+	
+	public void anadir(Cuenta c) {
+		lista.insertarNodo(c);
+	}
+	
 }
